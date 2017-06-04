30e2945b2ba229228bdc27dcb0fea35854f002bc
Angela Chieh <angela.chieh@gmail.com>
2017-06-04 17:25:09 +0200

    Erase console.logs

--- js/content.js
+++ js/content.js
@@ -1,6 +1,3 @@
-
-console.log("youtube controls ftw!");
-
 var videos = document.getElementsByTagName("video");
 var n = videos.length;
 
@@ -9,41 +6,29 @@ function action(type, p) {
 
 		case "rewind":
 			if (!p) p = 10;	
-			console.log("rewinding by " + p + " seconds, baby!");
 			for (var i = 0; i < n; i++) {
-				console.log("current time: " + videos[i].currentTime);
 				videos[i].currentTime -= p;
-				console.log("current time: " + videos[i].currentTime);
 			}
 			break;
 
 		case "forward":
 			if (!p) p = 10;	
-			console.log("fast forwarding by " + p + " seconds, baby!");
 			for (var i = 0; i < n; i++) {
-				console.log("current time: " + videos[i].currentTime);
 				videos[i].currentTime += p;
-				console.log("current time: " + videos[i].currentTime);
 			}
 			break;
 
 		case "go":
 			if (!p) p = 50;	
-			console.log("going to " + p + " seconds, baby!");
 			for (var i = 0; i < n; i++) {
-				console.log("current time: " + videos[i].currentTime);
 				videos[i].currentTime = p;
-				console.log("current time: " + videos[i].currentTime);
 			}
 			break;
 
 		case "setrate":
 			if (!p) p = 1;	
-			console.log("setting rate to " + p + ", baby!");
 			for (var i = 0; i < n; i++) {
-				console.log("playback rate: " + videos[i].playbackRate);
 				videos[i].playbackRate = p;
-				console.log("playback rate: " + videos[i].playbackRate);
 			}
 			break;
 
